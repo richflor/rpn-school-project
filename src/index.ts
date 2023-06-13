f58887d5229fda7702a0941f2d1353dc0f179380
richflor <fld.richard@gmail.com>
2023-06-13 16:05:35 +0200

    removed StackArray class that did not add anything

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,76 @@
-class StackArray{
-  Stack = [];
-  peek: number;
-  length:number
-  constructor(){
-      this.Stack = []
-      this.length = 0
-  }
+const rpn = (expression : string) => {
+    let expr = removeNegate(splitExpresion(expression));
+    let stackOperations:string|number[] = [];
 
-  inCome(value : number){
-      this.Stack.push(value)
-      this.peek = value
-      this.length +=1
-  }
-
-  outCome(){
-      let tmp = this.Stack.pop()
-      this.peek = this.Stack[this.Stack.length - 1]
-      this.length -=1
-      return tmp
-  }
-
-  getPeek(){
-      return this.peek
-  }
-}
-
-function splitExpresion(expression : string):any{
-  return expression.split(' ')
-}
-
-function noNegate(tabExpr:[]):any{
-  let tmp = []
-  for (let i = 0; i < tabExpr.length; i++) {
-    if (tabExpr[i] !== "NEGATE") {
-      tmp.push(tabExpr[i])
-    }else{
-      tmp[i-1] =  "-"+tmp[i-1]
+    try {
+        for (let i = 0; i < expr.length; i++) {
+            if (isNaN(expr[i])) {
+                console.log("no ", expr[i]);
+                let a = Number(stackOperations.pop());
+                let b = Number(stackOperations.pop());
+                let c = compute(b, a, expr[i]);
+                if (c === Infinity) {
+                    return "?";
+                }
+                stackOperations.push(c);
+            } else {
+                console.log("yes ", expr[i]);
+                stackOperations.push(expr[i]);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return error;
     }
-  }
-  return tmp
-}
 
-const computeSingle = (b:number,a:number,operator:string):number =>{
-  switch (operator) {
-    case "*":
-      return b*a
-    break;
-    case "-":
-      return b-a
-    break;
-    case "+":
-      return b+a
-    break;
-    case "/":
-      return b/a
-    break;
-    case "MOD":
-      return b%a
-    break;
-
-    default:
-      break;
-  }
-}
-
-const rpn = (expression: string) => {
-let expr = noNegate(splitExpresion(expression))
-let stackOperande = new StackArray()
-
-try {
-  for (let i = 0; i < expr.length; i++) {
-    if(isNaN(expr[i])){
-      console.log("no ",expr[i])
-      let a = Number(stackOperande.outCome())
-      let b = Number(stackOperande.outCome())
-      let c = computeSingle(b,a,expr[i])
-      if (c === Infinity){
-        return '?'
-      }
-      stackOperande.inCome(c)
+    if (stackOperations.length != 1) {
+        return "INVALID";
     }
-    else{
-      console.log("yes ",expr[i])
-      stackOperande.inCome(expr[i])
-    }
-  }
-} catch (error) {
-  console.log(error)
-  return error
+
+    return peek(stackOperations);
+};
+
+function peek(array:string|number[]):number|string {
+    return array[array.length - 1]
 }
 
-if (stackOperande.length != 1) {
-  return "INVALID"
+function splitExpresion(expression : string): string[] {
+    return expression.split(" ");
 }
-return stackOperande.getPeek()
+
+function removeNegate(tabExpr : string[]): any {
+    let tmp = [];
+    for (let i = 0; i < tabExpr.length; i++) {
+        if (tabExpr[i] !== "NEGATE") {
+            tmp.push(tabExpr[i]);
+        } else {
+            tmp[i - 1] = "-" + tmp[i - 1];
+        }
+    }
+    return tmp;
+}
+
+const compute = (b : number, a : number, operator : string) : number => {
+    switch (operator) {
+        case "*":
+            return b * a;
+            break;
+        case "-":
+            return b - a;
+            break;
+        case "+":
+            return b + a;
+            break;
+        case "/":
+            return b / a;
+            break;
+        case "MOD":
+            return b % a;
+            break;
+
+        default:
+            break;
+    }
 };
 
 export default rpn;
